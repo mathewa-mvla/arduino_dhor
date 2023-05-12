1c89fc4a45e46dac3f5a2538bc956d09b361b513
laurenh25516 <131912573+laurenh25516@users.noreply.github.com>
2023-05-12 12:09:12 -0700

    Add files via upload

--- Transmitter.ino
+++ Transmitter.ino
@@ -5,24 +5,20 @@ RCSwitch mySwitch = RCSwitch();
 void setup() {
   Serial.begin(9600);
   pinMode(7,INPUT);
-  pinMode(8, OUTPUT);
-  pinMode(12, OUTPUT);
 
-  mySwitch.enableTransmit(13);
+
+  mySwitch.enableTransmit(10);
   mySwitch.setPulseLength(165);
   Serial.println("starting");
 }
 
 void loop() {
-  digitalWrite(12,HIGH);
   if (digitalRead(7) == HIGH){
     Serial.println("button pressed");
-    digitalWrite(4,HIGH);
     mySwitch.send("Hello World");
     Serial.println("transmitting...");
   }
   else{
-    digitalWrite(4,LOW);
     Serial.println("button off");
   }
   delay(100);
